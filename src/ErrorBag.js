7d6bec9814664c3ef4bd267d4f72f7d861264798
Kostiyantyn Bozhko <bks@exyplis.com>
2020-09-08 14:11:01 +0300

    Quick fix.

--- src/ErrorBag.js
+++ src/ErrorBag.js
@@ -1,63 +1,63 @@
 class ErrorBag {
-  /**
-   * Create new ErrorBag instance
-   */
+    /**
+     * Create new ErrorBag instance
+     */
     constructor() {
         this.message = null;
         this.errors = {};
     }
 
-  /**
-   * Determine if an error exists for a given field
-   * @param {string} field
-   */
+    /**
+     * Determine if an error exists for a given field
+     * @param {string} field
+     */
     has(field) {
         return this.errors.hasOwnProperty(field);
     }
 
-  /**
-   * Determine if we have any errors.
-   */
+    /**
+     * Determine if we have any errors.
+     */
     any() {
         return Object.keys(this.errors).length > 0;
     }
 
-  /**
-   * Retrieve the error message for a field.
-   *
-   * @param {string} field
-   */
+    /**
+     * Retrieve the error message for a field.
+     *
+     * @param {string} field
+     */
     get(field) {
         if (this.errors[field]) {
             return this.errors[field][0];
         }
     }
 
-  /**
-   * Record the new errors.
-   *
-   * @param {object} errors
-   */
+    /**
+     * Record the new errors.
+     *
+     * @param {object} errors
+     */
     record(errors) {
         this.errors = errors;
     }
-  /**
-   * Record errors from axios response
-   *
-   * @param {Object} response
-   */
-    capture({ response }) {
+    /**
+     * Record errors from axios response
+     *
+     * @param {Object} response
+     */
+    capture(response) {
         this.message = response.data.message || response.statusText;
         if (response.data.hasOwnProperty('errors')) {
             this.record(response.data.errors);
         }
     }
 
-  /**
-   * Clear one or all error fields.
-   *
-   * @param {string|null} field
-   */
+    /**
+     * Clear one or all error fields.
+     *
+     * @param {string|null} field
+     */
     clear(field) {
         if (field) {
             delete this.errors[field];
@@ -68,16 +68,16 @@ class ErrorBag {
         this.clearMessage();
     }
 
-  /**
-   * Determine is there any message available
-   */
+    /**
+     * Determine is there any message available
+     */
     hasMessage() {
         return this.message !== null;
     }
 
-  /**
-   * Clear main message
-   */
+    /**
+     * Clear main message
+     */
     clearMessage() {
         this.message = null;
     }
